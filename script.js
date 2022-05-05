a0082d001f578ebfc837c99dd2e040aaf1927895
Dani1324 <danieleronchini2@gmail.com>
2022-05-05 22:39:11 +0200

    1.20

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ AOS.init({
 });
 
 // Write html lines
-var iOS = !window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent);
-if (iOS){
-  $('#mail1').removeAttr("href");
-  document.getElementById("mail1").style.color = "#000075";
-  $('#mail2').removeAttr("href");
-  document.getElementById("mail2").style.color = "#000075";
-  $('#mail3').removeAttr("href");
-  document.getElementById("mail3").style.color = "#000075";
-  $('#mail4').removeAttr("href");
-  document.getElementById("mail4").style.color = "#000075";
-}
+//var iOS = !window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent);
+//if (iOS){
+//  $('#mail1').removeAttr("href");
+//  document.getElementById("mail1").style.color = "#000075";
+//  $('#mail2').removeAttr("href");
+//  document.getElementById("mail2").style.color = "#000075";
+//  $('#mail3').removeAttr("href");
+//  document.getElementById("mail3").style.color = "#000075";
+//  $('#mail4').removeAttr("href");
+//  document.getElementById("mail4").style.color = "#000075";
+//}
 
 // Mobile optimization
 const width = window.innerWidth > 0 ? window.innerWidth : screen.width;
